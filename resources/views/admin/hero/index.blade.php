a196e267be99a227c37db4a2b22e27bf55ab7ef9
HumaCode <humaidizakaria1998@gmail.com>
2024-06-06 21:14:37 +0700

    section hero update part 1 - backend

--- resources/views/admin/hero/index.blade.php
+++ resources/views/admin/hero/index.blade.php
@@ -20,11 +20,48 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Update Profile</h4>
+                            <h4>Update Hero</h4>
                         </div>
                         <div class="card-body">
 
+                            <form action="">
 
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Background <span
+                                            class="text-danger">*</span></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div id="image-preview" class="image-preview banner-preview">
+                                            <label for="image-upload-2" id="image-label">Choose File</label>
+                                            <input type="file" name="banner" id="image-upload" />
+                                            {{-- <input type="hidden" name="old_banner" value="{{ $user->banner }}"> --}}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title <span
+                                            class="text-danger">*</span></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text" class="form-control" name="title">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"> Sub Title <span
+                                            class="text-danger">*</span></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text" class="form-control" name="sub_title">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <button type="submit" class="btn btn-primary">Update Hero</button>
+                                    </div>
+                                </div>
+
+                            </form>
 
                         </div>
                     </div>
@@ -34,3 +71,20 @@
         </div>
     </section>
 @endsection
+
+@push('style')
+    <script src="{{ asset('admin') }}/assets/modules/upload-preview/assets/js/jquery.uploadPreview.min.js"></script>
+
+
+    <script>
+        $.uploadPreview({
+            input_field: "#image-upload", // Default: .image-upload
+            preview_box: "#image-preview", // Default: .image-preview
+            label_field: "#image-label", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+    </script>
+@endpush

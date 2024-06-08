8e63e33dbb5e228dff3ce29ac5a9ecba1e6dfc70
HumaCode <humaidizakaria1998@gmail.com>
2024-06-08 14:59:42 +0700

    breadcrumb dynamis - backend

--- resources/views/admin/hero/index.blade.php
+++ resources/views/admin/hero/index.blade.php
@@ -15,8 +15,7 @@
             <h1>Update Hero</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="{{ route('admin.dashboard.index') }}">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="{{ route('admin.profile') }}">Profile</a></div>
-                <div class="breadcrumb-item">Update Profile</div>
+                <div class="breadcrumb-item">Hero</div>
             </div>
         </div>
 

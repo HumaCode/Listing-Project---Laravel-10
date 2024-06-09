a8d6d6df18bddc9bfafa47e06fa78859cedde129
HumaCode <humaidizakaria1998@gmail.com>
2024-06-09 20:54:38 +0700

    making sidebar active - backend

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -204,21 +204,17 @@
                 </ul>
             </li>
 
-            <li
-                class="dropdown {{ setSidebarActive(['admin.category.index', 'admin.category.create', 'admin.category.edit', 'admin.location.index', 'admin.location.create', 'admin.location.edit', 'admin.amenity.index', 'admin.amenity.create', 'admin.amenity.edit']) }}">
+            <li class="dropdown {{ setSidebarActive(['admin.category.*', 'admin.location.*', 'admin.amenity.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
                         class="fas fa-columns"></i> <span>Listing</span></a>
                 <ul class="dropdown-menu">
-                    <li
-                        class="{{ setSidebarActive(['admin.category.index', 'admin.category.create', 'admin.category.edit']) }}">
+                    <li class="{{ setSidebarActive(['admin.category.*']) }}">
                         <a class="nav-link" href="{{ route('admin.category.index') }}">Category</a>
                     </li>
-                    <li
-                        class="{{ setSidebarActive(['admin.location.index', 'admin.location.create', 'admin.location.edit']) }}">
+                    <li class="{{ setSidebarActive(['admin.location.*']) }}">
                         <a class="nav-link" href="{{ route('admin.location.index') }}">Location</a>
                     </li>
-                    <li
-                        class="{{ setSidebarActive(['admin.amenity.index', 'admin.amenity.create', 'admin.amenity.edit']) }}">
+                    <li class="{{ setSidebarActive(['admin.amenity.*']) }}">
                         <a class="nav-link" href="{{ route('admin.amenity.index') }}">Amenities</a>
                     </li>
                 </ul>

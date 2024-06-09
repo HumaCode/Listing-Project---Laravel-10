3c6e126051bec71089ec1a42bbd8b3caa247696e
HumaCode <humaidizakaria1998@gmail.com>
2024-06-09 19:13:02 +0700

    amenity - backend

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -205,16 +205,22 @@
             </li>
 
             <li
-                class="dropdown {{ setSidebarActive(['admin.category.index', 'admin.category.create', 'admin.category.edit', 'admin.location.index', 'admin.location.create', 'admin.location.edit']) }}">
+                class="dropdown {{ setSidebarActive(['admin.category.index', 'admin.category.create', 'admin.category.edit', 'admin.location.index', 'admin.location.create', 'admin.location.edit', 'admin.amenity.index', 'admin.amenity.create', 'admin.amenity.edit']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
                         class="fas fa-columns"></i> <span>Listing</span></a>
                 <ul class="dropdown-menu">
                     <li
                         class="{{ setSidebarActive(['admin.category.index', 'admin.category.create', 'admin.category.edit']) }}">
-                        <a class="nav-link" href="{{ route('admin.category.index') }}">Category</a></li>
+                        <a class="nav-link" href="{{ route('admin.category.index') }}">Category</a>
+                    </li>
                     <li
                         class="{{ setSidebarActive(['admin.location.index', 'admin.location.create', 'admin.location.edit']) }}">
-                        <a class="nav-link" href="{{ route('admin.location.index') }}">Location</a></li>
+                        <a class="nav-link" href="{{ route('admin.location.index') }}">Location</a>
+                    </li>
+                    <li
+                        class="{{ setSidebarActive(['admin.amenity.index', 'admin.amenity.create', 'admin.amenity.edit']) }}">
+                        <a class="nav-link" href="{{ route('admin.amenity.index') }}">Amenities</a>
+                    </li>
                 </ul>
             </li>
 
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AmenityController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\HeroController;
@@ -33,6 +34,9 @@ Route::group(['middleware' => ['auth', 'user.type:admin'], 'prefix' => 'admin', 
 
     // location routes
     Route::resource('/location', LocationController::class);
+
+    // amenity routes
+    Route::resource('/amenity', AmenityController::class);
 });
 
 // slug route
